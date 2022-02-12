bd13e4454fc156a80eac14ffc73d654b165f63ce
PhysX-42 <batuhanozturk2020@outlook.com>
2022-02-12 13:41:32 -0500

    Practice

--- src/com/Java/firstExample.java
+++ src/com/Java/firstExample.java
@@ -3,7 +3,26 @@ package com.Java;
 public class firstExample {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
+		System.out.println("Hello World");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 
 	}
 
